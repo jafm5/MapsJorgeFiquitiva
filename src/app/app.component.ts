c124b986a2559a061eb097e09bd4ec0a45b03980
Jorge <jorge_fik@outlook.com>
2021-10-07 11:18:37 -0500

    se implementa marcador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MapsJorgeFiquitiva';
+  position = {
+    lat: -34.681,
+    lng: -58.371
+  };
+
+  label= {
+    color: 'red',
+    text: 'Tu dirección'
+  };
 }
