c213c63b4e9b167cab1cddba57c38698459872fd
Jorge <jorge_fik@outlook.com>
2021-10-07 13:17:00 -0500

    se ajustan marcadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  lat: number;
+  lng: number;
+  zoom: number;
+
+  constructor() {
+    this.lat = 4.801267;
+    this.lng = -74.118113;
+    this.zoom = 6;
+
+  }
+
+  posicionActual(){
+    navigator.geolocation.getCurrentPosition(position => { 
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+
+    })
+
+  }
+
+
   title = 'MapsJorgeFiquitiva';
+
   position = {
-    lat: -34.681,
-    lng: -58.371
+    lat: 4.801267,
+    lng: -74.118113,
   };
 
   label= {
     color: 'red',
     text: 'Tu dirección'
   };
+
+
 }
