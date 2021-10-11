05c24996f4cd2c1021b94b40592c305c50c9e31a
Jorge <jorge_fik@outlook.com>
2021-10-10 22:38:30 -0500

    creación de array y diferentes marcadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
+import { Address } from 'ngx-google-places-autocomplete/objects/address';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  title = 'MapsJorgeFiquitiva';
+// ]]@ViewChild("placesRef") placesRef : GooglePlaceDirective;
+
+  ngOnInit(){
+
+  }
+
+  public handleAddressChange(addres: Address){
+
+    console.log(addres);
+
+  }
 
   lat: number;
   lng: number;
   zoom: number;
 
+
+  locations = [
+    { lat: 4.801267, lng: -74.118113 },
+    { lat: 4.701267, lng: -74.118113 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: -33.848588, lng: 151.209834 },
+    { lat: -33.851702, lng: 151.216968 },
+    { lat: -34.671264, lng: 150.863657 },
+    { lat: -35.304724, lng: 148.662905 },
+    { lat: -36.817685, lng: 175.699196 },
+    { lat: -36.828611, lng: 175.790222 },
+    { lat: -37.75, lng: 145.116667 },
+    { lat: -37.759859, lng: 145.128708 },
+    { lat: -37.765015, lng: 145.133858 },
+    { lat: -37.770104, lng: 145.143299 },
+    { lat: -37.7737, lng: 145.145187 },
+    { lat: -37.774785, lng: 145.137978 },
+    { lat: -37.819616, lng: 144.968119 },
+    { lat: -38.330766, lng: 144.695692 },
+    { lat: -39.927193, lng: 175.053218 },
+    { lat: -41.330162, lng: 174.865694 },
+    { lat: -42.734358, lng: 147.439506 },
+    { lat: -42.734358, lng: 147.501315 },
+    { lat: -42.735258, lng: 147.438 },
+    { lat: -43.999792, lng: 170.463352 },
+  ];
+
+
   constructor() {
     this.lat = 4.801267;
     this.lng = -74.118113;
     this.zoom = 6;
-
   }
+
 
   posicionActual(){
     navigator.geolocation.getCurrentPosition(position => { 
@@ -26,14 +69,13 @@ export class AppComponent {
     })
 
   }
-
-
-  title = 'MapsJorgeFiquitiva';
-
+ 
   position = {
-    lat: 4.801267,
-    lng: -74.118113,
+    lat: 4.801267, 
+    lng: -74.118113
   };
+
+
 
   label= {
     color: 'red',
